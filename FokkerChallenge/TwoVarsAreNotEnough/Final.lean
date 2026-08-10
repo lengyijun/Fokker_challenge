85dbdd3ab67c32cde5dfef3c803200d5bcc4eea5
lengyijun <sjtu5140809011@gmail.com>
2026-08-11 06:04:13 +0800

    wip

--- FokkerChallenge/TwoVarsAreNotEnough/Final.lean
+++ FokkerChallenge/TwoVarsAreNotEnough/Final.lean
@@ -45,11 +45,11 @@ theorem final {n M}
   have := Relation.Normal.reflTransGen_eq h_betaeta_nf hz1
   subst Z
   have eta_steps : beta_nf ↠ηᶠ List.foldl app (fvar "x") [(fvar "y").app (H n)] := beta_eta_star_of_beta_normal h_beta_nf hz2
-  obtain ⟨i, l, beta_nf_eq⟩ := betaNF_etaStar_absN_spine beta_nf "x" [(fvar "y").app (H n)] h_beta_nf eta_steps
-  -- TODO: we need to prove "y" in every element in l
+  have beta_nf_lc : beta_nf.LC := by cases FullBeta.steps_lc_or_rfl beta_steps <;> grind
+  obtain ⟨i, E, l, beta_nf_eq, _, _, _, _, _, _⟩ := betaNF_etaStar_shape_len_one_openDown_fv beta_nf_lc h_beta_nf eta_steps
+
   have h1 := iterate_app i beta_steps (LC.fvar "y")
   rw [beta_nf_eq] at h1
-  have beta_nf_lc : beta_nf.LC := by cases FullBeta.steps_lc_or_rfl beta_steps <;> grind
   obtain ⟨l, h2⟩ := redex_n_apps_n_abs_of_apps i "x" "y" l (by grind)
   have recursive_app_lc : (List.foldl app (fvar "x") l).LC := by
     cases FullBeta.steps_lc_or_rfl (h1.trans h2) with
