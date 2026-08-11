91dde6d90c97f5678318c5d0b107bd621a00d9fc
lengyijun <sjtu5140809011@gmail.com>
2026-08-11 14:39:19 +0800

    generalize

--- FokkerChallenge/EnhancedCslib/FlipApp.lean
+++ FokkerChallenge/EnhancedCslib/FlipApp.lean
@@ -10,15 +10,16 @@ namespace Cslib
 
 namespace LambdaCalculus.LocallyNameless.Untyped.Term
 
-lemma step_flip_app_l {M M'} {Ns : List (Term String)} (steps : M ⭢βᶠ M') (lc_Ns : ∀ N ∈ Ns, LC N) :
-    Ns.foldl (flip app) M ⭢βᶠ Ns.foldl (flip app) M' := by
+lemma step_flip_app_l {R} {M M'} {Ns : List (Term String)} (steps : Xi R M M') (lc_Ns : ∀ N ∈ Ns, LC N) :
+    Xi R (Ns.foldl (flip app) M)  (Ns.foldl (flip app) M') := by
   induction Ns generalizing M M' with
   | nil => grind
   | cons head tail ih =>  simp [flip]
                           apply ih <;> grind
 
-lemma steps_flip_app_l {M M'} {Ns : List (Term String)} (steps : M ↠βᶠ M') (lc_Ns : ∀ N ∈ Ns, LC N) :
-    Ns.foldl (flip app) M ↠βᶠ Ns.foldl (flip app) M' := by
+lemma steps_flip_app_l {R} {M M'} {Ns : List (Term String)} (steps : Relation.ReflTransGen (Xi R) M M')
+    (lc_Ns : ∀ N ∈ Ns, LC N) :
+    Relation.ReflTransGen (Xi R)  (Ns.foldl (flip app) M) (Ns.foldl (flip app) M') := by
   induction steps <;> grind [step_flip_app_l]
 
 lemma flip_app_fv {M} {Ns : List (Term String)}:
